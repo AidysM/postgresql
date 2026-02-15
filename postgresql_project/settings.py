f1655fb7533fcd690729d1aed7f85df969e3381d
AidysM <mongush10011979@gmail,com>
2026-02-15 21:37:22 +0300

    2 commit

--- postgresql_project/settings.py
+++ postgresql_project/settings.py
@@ -72,10 +72,21 @@ WSGI_APPLICATION = 'postgresql_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db', 
+        'PORT': 5432
     }
 }
 
